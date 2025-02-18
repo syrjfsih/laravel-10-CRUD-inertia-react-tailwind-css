0d06c95477505ddef7df3598313bc29af8b1e98c
syrjfsih <rajzchanel@gmail.com>
2025-02-18 10:11:47 +0700

    Menambbhakan Route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -28,6 +29,7 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+Route::resource('posts', PostController::class)->middleware(['auth', 'verified']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -36,3 +38,9 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+//Baris yang ditambahkan adalah use App\Http\Controllers\PostController; dan Route::resource('posts', //PostController::class)->middleware(['auth', 'verified']);
+//use App\Http\Controllers\PostController;: Ini adalah pernyataan yang mengimpor (menggunakan) kelas PostController yang //terletak dalam namespace App\Http\Controllers. Dengan cara ini, kita dapat menggunakannya dalam definisi rute tanpa harus //menuliskan namespace lengkapnya.
+//Route::resource('posts', PostController::class)->middleware(['auth', 'verified']);: Ini adalah definisi rute yang melakukan beberapa hal:
+//[-] Route::resource('posts', PostController::class): Ini menghasilkan berbagai rute standar yang diperlukan untuk //mengelola posting, seperti rute untuk menampilkan, membuat, menyimpan, mengedit, dan menghapus posting. Ini adalah metode //singkat untuk membuat rute CRUD (Create, Read, Update, Delete) dalam Laravel.
+//[-] ->middleware(['auth', 'verified']): Middleware adalah filter yang digunakan untuk memproses permintaan sebelum //mencapai tindakan yang sesuai di PostController. Dalam hal ini, auth memastikan bahwa pengguna harus masuk (authenticated) //untuk mengakses rute-rute ini, dan verified memastikan bahwa pengguna juga harus memiliki email mereka diverifikasi //sebelum dapat menggunakannya.
